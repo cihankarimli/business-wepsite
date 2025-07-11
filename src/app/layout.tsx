2550564e4d6c6914056e1b3f6c6d3d5371628da9
cihankarimli <kerimlicihan1@gmail.com>
2025-07-12 01:57:37 +0400

    ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Roboto_Flex } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/feutures/header/Navbar";
+import Navbar from "@/features/header/Navbar";
 import { SmoothScrollProvider } from "@/components/smoothScrool/SmoothScrool";
-import Footer from "@/feutures/footer/Footer";
+import Footer from "@/features/footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import DigitalSolitionSection from "@/feutures/mainSections/DigitalSolition";
-import OurServicesSection from "@/feutures/mainSections/OurServices";
-import OurWorksSection from "@/feutures/mainSections/OurWorksSection";
-import ReasonsSection from "@/feutures/mainSections/ReasonsSection";
-import Testimonials from "@/feutures/mainSections/Testimonials";
-import AskQuestionSection from "@/feutures/mainSections/AskQuestionSection";
+import DigitalSolitionSection from "@/features/mainSections/DigitalSolition";
+import OurServicesSection from "@/features/mainSections/OurServices";
+import OurWorksSection from "@/features/mainSections/OurWorksSection";
+import ReasonsSection from "@/features/mainSections/ReasonsSection";
+import Testimonials from "@/features/mainSections/Testimonials";
+import AskQuestionSection from "@/features/mainSections/AskQuestionSection";
 
 export default function Home() {
   return (
